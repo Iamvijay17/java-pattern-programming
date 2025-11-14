29c5543f7548b47fe53456028dcab5bfb130e326
iamvijay17 <vijay9578979255@gmail.com>
2025-11-14 10:33:45 +0530

    Square Pattern

---
+++ src/SquarePattern.java
@@ -0,0 +1,19 @@
+/*
+         *****   ===> 1st row - 5 col - 5 star
+         *****   ===> 2nd row - 5 col - 5 star
+         *****   ===> 3rd row - 5 col - 5 star
+         *****   ===> 4th row - 5 col - 5 star
+ */
+
+//  4 row 5 col
+
+public class SquarePattern {
+        public static void main(String arg[]) {
+                for (int i = 0; i < 4; i++) {
+                        for (int j = 0; j < 5; j++) {
+                                System.out.print("*");
+                        }
+                        System.out.println();
+                }
+        }
+}
